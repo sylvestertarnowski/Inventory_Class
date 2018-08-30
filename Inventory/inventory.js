765962c5c989db61274ddd4ed32c8e533b9b143c
sylvestertarnowski <34459277+sylvestertarnowski@users.noreply.github.com>
2018-08-30 20:53:25 +0200

    changed the structure of constructor functions
    changed to class system

--- Inventory/inventory.js
+++ Inventory/inventory.js
@@ -1,20 +1,39 @@
-/* Product Inventory Project - Create an application which manages an 
-inventory of products. Create a product class which has a price, id, 
+/* Product Inventory Project - Create an application which manages an
+inventory of products. Create a product class which has a price, id,
 and quantity on hand. Then create an inventory class which keeps track
 of various products and can sum up the inventory value. */
+var inventory = [];
 
+class Product {
+  constructor(name, id, price, units) {
+    this.name = name;
+    this.id = id;
+    this.price = price;
+    this.units = units;
+    // this.removeUnits = function(val) {
+    //     this.units = this.units - val;
+    // },
+    // this.dropPriceByPercentage = function(val) {
+    //     this.price = this.price - (this.price * val);
+    // }
+    inventory.push(this);
+  }
 
-function Product(name, id, price, units) {
-    this.name = name,
-    this.id = id,
-    this.price = price,
-    this.units = units,
-    this.removeUnits = function(val) {
-        this.units = this.units - val;
-    },
-    this.dropPriceByPercentage = function(val) {
-        this.price = this.price - (this.price * val);
-    }
+  removeUnits(val) {
+    this.units = this.units - val;
+  }
+
+  dropPriceByPercentage(val) {
+    this.price = this.price - (this.price * val);
+  }
+
+  addUnits(val) {
+    this.units = this.units + val;
+  }
+  
+  get what() {
+    console.log(this.name + " nalezy do kategorii: " + this.id);
+  }
 }
 
 let chlebRycerski = new Product("Chleb Rycerski", "Chleb", 3.99, 20);
@@ -25,14 +44,14 @@ let colgate = new Product("Colgate", "Pasta do zebow", 4.99, 20);
 let butyVans = new Product("Nowiutkie Vansy", "Buty", 199.99, 10);
 let butyAdidas = new Product("Nowiutkie Adidasy", "Buty", 299.99, 15);
 
-let inventory = [chlebRycerski,
-     chlebZytni,
-     chlebOrkiszowy,
-     signal,
-     colgate,
-     butyVans,
-     butyAdidas
-];
+// let inventory = [chlebRycerski,
+//      chlebZytni,
+//      chlebOrkiszowy,
+//      signal,
+//      colgate,
+//      butyVans,
+//      butyAdidas
+// ];
 
 function countInventoryValue() {
     var totalValue = 0;
@@ -42,5 +61,13 @@ function countInventoryValue() {
     return "$" + totalValue.toFixed(2);
 }
 
-console.log(chlebRycerski);
+function inventoryCount() {
+    let totalCount = 0;
+    for (let n = 0; n < inventory.length; n++) {
+        totalCount = totalCount + inventory[n].units;
+    }
+    return 'The total count of inventory is ' + totalCount;
+}
+
+console.log(inventoryCount());
 console.log(countInventoryValue());
